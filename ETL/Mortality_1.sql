128ed272bf53c6e6adca9701751697766bb7de0f
Merari Cisneros <meraridanielcisneros@gmail.com>
2021-08-19 23:12:52 -0700

    Added Postgres script to generate table for first Dataset from WHO

---
+++ ETL/Mortality_1.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE Mortality_1 (
+	Country VARCHAR(50) CONSTRAINT firstkey PRIMARY KEY ,
+	Admin1 VARCHAR(50) , 
+	Subdiv VARCHAR(50) ,
+	Year VARCHAR(50) , 
+	list VARCHAR(50) , 
+	Cause VARCHAR(50) ,
+	Sex VARCHAR(50) ,
+	Frmat VARCHAR(50) ,
+	IM_Frmat VARCHAR(50) ,
+	Deaths1 VARCHAR(50) 
+
+);
